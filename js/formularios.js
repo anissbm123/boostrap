1018e37b105307077ce22a066ef7c04ae2496d3d
John Doe <anasofiabarretomolano@gmail.com>
2021-10-20 16:47:06 -0500

    primer compromiso

---
+++ js/formularios.js
@@ -0,0 +1,15 @@
+alert ( 
+    "Hola, vamos a conoser mas sobre ti"
+)
+function recibirdatos(){
+  
+    let favorito = document.getElementById("favorito").value
+    let nombre = document.getElementById("nombre").value
+    let vistos = document.getElementById("vistos").value
+    let mascotas = document.getElementById("mascotas").value
+   
+    if(favorito == "fargan")
+        alert ("Hola bienvenido " + nombre + " si te gusta fargan te recomiendo verte tambien la perspectiva de auron y alexby ya que en ellas tambien sale ")
+    }
+
+}
